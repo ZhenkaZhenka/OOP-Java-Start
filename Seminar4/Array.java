eb3309546120c45cedf4a72242901281a19cfa11
Evgenii <eaturkin@yandex.ru>
2023-03-19 22:58:59 +0300

    min, max - methods have been added

--- Seminar4/Array.java
+++ Seminar4/Array.java
@@ -4,65 +4,97 @@ public class Array<E> {
     private E[] array;
     private int size;
     private Object[] defaultEmptyArray = {};
-    public Array(){
+
+    public Array() {
         array = (E[]) defaultEmptyArray;
     }
-    public Array(E[] col){
+
+    public Array(E[] col) {
         array = col;
         size = array.length;
     }
-    public void add(E obj){
-        int newSize = array.length +1 ;
+
+    public void add(E obj) {
+        int newSize = array.length + 1;
         E[] newArray;
         newArray = Arrays.copyOf(array, newSize);
         newArray[array.length] = obj;
-        array =  Arrays.copyOf(newArray, newSize);
+        array = Arrays.copyOf(newArray, newSize);
         size += 1;
     }
-    public void delete(int index){
+
+    public void delete(int index) {
         int counter = 0;
-        int newSize = array.length -1;
-        E[] temp = (E[])new Object[newSize];
-        for (int i = 0; i <= newSize-1; i++) {
-            if(i == index){counter++;}
+        int newSize = array.length - 1;
+        E[] temp = (E[]) new Object[newSize];
+        for (int i = 0; i <= newSize - 1; i++) {
+            if (i == index) {
+                counter++;
+            }
             temp[i] = array[i + counter];
         }
         array = Arrays.copyOf(temp, newSize);
         size -= 1;
     }
-    public void delete(E obj){
-        for (int i = 0; i < array.length; i+=1) {
-            if(array[i].equals(obj)){
+
+    public void delete(E obj) {
+        for (int i = 0; i < array.length; i += 1) {
+            if (array[i].equals(obj)) {
                 delete(i);
                 delete(obj);
             }
         }
     }
-    public int size(){
+    public <E extends Comparable<E>> E min(){
+        E min = (E)array[0];
+        for (int i = 1; i < size; i++) {
+            if (min.compareTo((E)array[i]) > 0){
+                min = (E)array[i];
+            }
+        }
+        return min;
+    }
+    public <E extends Comparable<E>> E max(){
+        E max = (E)array[0];
+        for (int i = 1; i < size; i++) {
+            if (max.compareTo((E)array[i]) < 0){
+                max = (E)array[i];
+            }
+        }
+        return max;
+    }
+
+    public int size() {
         return size;
     }
-    public <E> int indexOf(E obj){
-        for (int i = 0; i < size; i++){
-            if (array[i].equals(obj)){
+
+    public <E> int indexOf(E obj) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(obj)) {
                 return i;
             }
         }
         return -1;
     }
-    public <E> boolean contain(E obj){
+
+    public <E> boolean contain(E obj) {
         for (var item : array) {
-            if (item.equals(obj)){return true;}
+            if (item.equals(obj)) {
+                return true;
+            }
         }
         return false;
     }
-    protected void print(){
+
+    protected void print() {
         for (var item : array) {
             System.out.print(item + " ");
         }
         System.out.println();
     }
-    protected <E> E valueOf(int index){
-        return (E)array[index];
+
+    protected <E> E valueOf(int index) {
+        return (E) array[index];
     }
 
 }
--- Seminar4/ProgArray.java
+++ Seminar4/ProgArray.java
@@ -3,19 +3,22 @@ import java.util.ArrayList;
 public class ProgArray {
 
     public static void main(String[] args) throws Exception {
-        Integer[] col = {5,4,3,4,5,6,33,2};
+        String[] col = {"fff","ytyyty","sfgdfg","jdhg","sgdfg333","xbsfsdfhrw"};
         Array<Double> ar = new Array();
         ar.print();
         ar.add(1.9);
-        ar.add(1.3);
-        ar.add(1.3);
-        ar.add(1.9);
+        ar.add(2.9);
+        ar.add(1.2);
+        ar.add(1.7);
         ar.add(1.3);
         ar.print();
-//        ar.delete(1.3);
-        System.out.println(ar.size());
+        Array<String> ar2 = new Array(col);
+        String temp = ar2.max();
+        System.out.println(temp);
+        Double num = ar.sum();
+        System.out.println(num);
         ar.print();
-        Array<Integer> ar2 = new Array(col);
+
         ar2.print();
 
 
